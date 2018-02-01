78e05a4efd1acd2aea814475b73b7a6f11b7d533
mrivera316 <35825749+mrivera316@users.noreply.github.com>
2018-01-31 23:47:34 -0600

    Add files via upload

---
+++ r2java.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+var instructorName=prompt("Who is your instructor?");
+
+switch(instructorName){
+	case"Viviane":
+	alert("Lucky!");
+	break;
+			
+	case"Susan":
+	alert("Even luckier!");
+	break;
+			
+	default:
+	alert("That is not a known Digital Media instructor.");
+}
+
+
+function myFunction() {
+    alert("Hooray!");
+}
+
+var x = math (4, 3);
+function math(a, b) {
+	return a * b;
+}
